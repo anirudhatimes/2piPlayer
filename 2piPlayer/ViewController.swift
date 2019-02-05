f4dfb02e606a106fa6380e68389f6dddd9a24003
Anirudha Tolambia <anirudha.tolambia@anirudha-mbp.local>
2019-02-05 12:07:13 +0530

    Initial Commit

---
+++ 2piPlayer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  2piPlayer
+//
+//  Created by Anirudha Tolambia on 05/02/19.
+//  Copyright © 2019 Anirudha Tolambia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
