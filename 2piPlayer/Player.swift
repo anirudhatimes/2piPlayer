9c6689e4cc3c3eeb83625c17aa940c874c6d23e1
Anirudha Tolambia <anirudha.tolambia@anirudha-mbp.local>
2019-02-05 12:09:05 +0530

    Lets start

---
+++ 2piPlayer/Player.swift
@@ -0,0 +1,13 @@
+//
+//  Player.swift
+//  2piPlayer
+//
+//  Created by Anirudha Tolambia on 05/02/19.
+//  Copyright © 2019 Anirudha Tolambia. All rights reserved.
+//
+
+import UIKit
+
+class Player: AVPlayer {
+
+}
